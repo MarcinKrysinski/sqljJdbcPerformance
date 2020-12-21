a581481f4467b8eae398b6125ec5f7ab850fd07d
Marcin <marcin.jakubkrysinski@gmail.com>
2020-12-21 20:23:42 +0100

    Initial Commit

---
+++ src/test/java/pl/krysinski/sqljdbc/SqljdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.krysinski.sqljdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqljdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
